fe2ec724ccd0c3e55e8f798395fa9339e457e277
Gae <michalkowalski876@gmail.com>
2025-05-11 11:45:08 +0200

    passWord Analyzation

--- Colections_excercises/Zadania.java
+++ Colections_excercises/Zadania.java
@@ -22,31 +22,31 @@ public class Zadania {
 
         boolean czyLiczba = false;
         boolean CzyDuzaLitera = false;
-        boolean znakSpecjalny = false;
+        boolean CzyZnakSpecjalny = false;
 
         for (int i = 0; i < passWord.length(); i++) {
-            char c = passWord.charAt(i);
-
-            if (Character.isDigit(c)) {
-                czyLiczba = true;
-            }
-
-            if (Character.isUpperCase(c)) {
-                CzyDuzaLitera = true;
-            }
-
-            if (!Character.isLetterOrDigit(c)) {
-                znakSpecjalny = true;
-            }
-
-            if (!rozneZnaki.contains(c)) {
-                rozneZnaki.add(c);
+            if (!rozneZnaki.contains(passWord.charAt(i))) {
+                rozneZnaki.add(passWord.charAt(i));
             }
         }
+        for (int i = 0; i < rozneZnaki.size(); i++) {
+            char znak = rozneZnaki.get(i);
 
-        boolean isStrong = rozneZnaki.size() >= 6 && czyLiczba && CzyDuzaLitera && znakSpecjalny;
+            if (Character.isDigit(znak)) {
+                czyLiczba = true;
+            }
+            if (Character.isUpperCase(znak)) {
+                CzyDuzaLitera = true;
+            }
+            if (!Character.isLetterOrDigit(znak)) {
+                CzyZnakSpecjalny = true;
+            }
 
-        if (isStrong) {
+        }
+
+        boolean czySilne = rozneZnaki.size() >= 6 && czyLiczba && CzyDuzaLitera && CzyZnakSpecjalny;
+
+        if (czySilne) {
             System.out.println("Hasło jest wystarczająco silne.");
         } else {
             System.out.println("Hasło musi spełniać następujące wymagania:");
@@ -59,11 +59,11 @@ public class Zadania {
             if (!CzyDuzaLitera) {
                 System.out.println("- Hasło musi zawierać przynajmniej jedną wielką literę.");
             }
-            if (!znakSpecjalny) {
+            if (!CzyZnakSpecjalny) {
                 System.out.println("- Hasło musi zawierać przynajmniej jeden znak specjalny.");
             }
         }
 
-        return isStrong;
+        return czySilne;
     }
 }
